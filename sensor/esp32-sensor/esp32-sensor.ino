4e2da5c4892f43b3d2271976c1a4e2514313fcbd
kode-git <mariosessa64@gmail.com>
2022-05-18 15:25:50 +0200

    Adding http support on esp32

--- sensor/esp32-sensor/esp32-sensor.ino
+++ sensor/esp32-sensor/esp32-sensor.ino
@@ -3,18 +3,19 @@
 #include <DHT.h> 
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <HTTPClient.h>
 #include <String.h>
 
  
-#define DHTPIN 27 // data pin location, can change during installation 
+#define DHTPIN 4 // Warning: data pin location, can change during installation 
 #define MQ2PIN 25
 
 // Protocol switching variables
-int prot_mode = 1;
+int prot_mode = 3;
 int smokeA0 = 17;
 // WiFi Data
-const char *ssid = "iPhone"; // Enter your WiFi name
-const char *password = "19951995";  // Enter WiFi password
+const char *ssid = "Vodafone-C01410160"; // Warning: enter your WiFi name
+const char *password = "PhzX3ZE9xGEy2H6L";  // Warning: enter WiFi password
 
 // MQTT Broker
 const char *mqtt_broker = "130.136.2.70";
@@ -25,6 +26,12 @@ const char *mqtt_username = "iot2020";
 const char *mqtt_password = "mqtt2020*";
 const int mqtt_port = 1883;
 
+// Http protocol
+int http_port = 8080;
+String http_hostname = "localhost";
+String pathname = "/update-sensor";
+
+
 // WiFi client declaration
 WiFiClient espClient;
 
@@ -34,6 +41,7 @@ PubSubClient client(espClient);
 // DHT22 sensor with setup on pin
 DHT dht_sensor(DHTPIN,DHT22); 
 float temperature, humidity; 
+
 
 void coap_connection(){
   // to-do: manage the same value trasmission on coap protocol
@@ -76,8 +84,10 @@ void setup() {
   Serial.println(WiFi.localIP());
   if(prot_mode == 1){
     mqtt_connection();
-  } else {
+  } else if(prot_mode == 2) {
     coap_connection();
+  } else if(prot_mode == 3){
+    // nothing, http did only WiFi connection
   }
  
 }
@@ -113,9 +123,33 @@ void loop() {
   int ret = snprintf(buffer_temp, sizeof buffer_temp, "%f", temperature);
   char buffer_hum[64];
   int ret2 = snprintf(buffer_hum, sizeof buffer_hum, "%f", humidity);
-  
-  client.publish(temperature_topic, buffer_temp);
-  client.publish(humidity_topic, buffer_hum);
+
+  if (prot_mode == 1){
+    // mqtt publish
+    client.publish(temperature_topic, buffer_temp);
+    client.publish(humidity_topic, buffer_hum);
+  } else if(prot_mode == 2){
+    // to-do, sending to coap
+  } else if(prot_mode == 3){
+    // http request didn't need setup() configuration except for WiFi connection.
+      
+    // check that the sensor is still connected
+    HTTPClient http;
+    // define get request
+    String http_path = "http://" + http_hostname + ":" + http_port + pathname + "?temp=" + ret + "?hum=" + ret2;
+    Serial.println("Sending" + http_path);
+    http.begin(http_path.c_str());
+
+    // send get http request
+    int httpResponseCode = http.GET();
+
+    // check response status
+    Serial.printf("Response status %d", &httpResponseCode);
+    // free() request
+    http.end();
+
+    
+  }
   
   Serial.println("--------------------------");
   delay(2000); 
